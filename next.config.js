d7bbeffc6bf421848e5f3d7dae4c9ea3d07cfc57
Rmonofee1122 <nakanishi_r@monofee.co.jp>
2025-01-06 14:47:38 +0900

    10th commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  output: 'export'
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+
+  //Other settings
 }
+ 
+module.exports = nextConfig
