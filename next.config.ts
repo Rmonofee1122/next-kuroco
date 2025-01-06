c576ad47a9f7bcdf89bc6e312ba9eb2a17df9024
Rmonofee1122 <nakanishi_r@monofee.co.jp>
2025-01-06 13:25:17 +0900

    second commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  // 2025/01/06 中西 
+  // 以下の部分を削除(https://kuroco.app/ja/quick-guide/#next-deploy-config)
+  output: 'export',
 };
 
 export default nextConfig;
